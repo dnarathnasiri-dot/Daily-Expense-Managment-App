6f2bbe8fd825fb427adc2d5a7be87472294dc37f
hiruunisansala <a.hiruunisansala@gmail.com>
2026-02-19 11:03:49 +0530

    register failed! update

--- dems-backend/handlers/auth/register.php
+++ dems-backend/handlers/auth/register.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../../includes/db.php';
 require_once __DIR__ . '/../../includes/auth.php';
 require_once __DIR__ . '/../../includes/response.php';
+require_once __DIR__ . '/../../includes/cors.php';
 
 $body      = getBody();
 $firstName = trim($body['first_name'] ?? '');
