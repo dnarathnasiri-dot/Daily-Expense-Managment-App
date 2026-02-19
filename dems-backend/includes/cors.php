57094beb79a3ac7ec84ca35825e6979734adbd4a
Chalukya Dilshan <chalukyad10@gmail.com>
2026-02-19 20:02:45 +0530

    hiatus

--- dems-backend/includes/cors.php
+++ dems-backend/includes/cors.php
@@ -1,5 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5173");
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+if (preg_match('/^http:\/\/localhost(:\d+)?$/', $origin)) {
+    header("Access-Control-Allow-Origin: $origin");
+}
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, Accept");
 header("Access-Control-Allow-Credentials: true");
--- dems-frontend/src/services/api.js
+++ dems-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost/dems-backend/handlers',
+  baseURL: 'http://localhost/dems-backend/handlers/auth',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
