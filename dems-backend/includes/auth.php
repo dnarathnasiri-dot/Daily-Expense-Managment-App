4663020d668fe36d82af5816d70771df410aa21b
Chalukya Dilshan <chalukyad10@gmail.com>
2026-02-19 11:35:30 +0530

    Update auth.php

--- dems-backend/includes/auth.php
+++ dems-backend/includes/auth.php
@@ -1,5 +1,5 @@
 <?php
-require_once _DIR_ . '/../config/database.php';
+require_once __DIR__ . '/../config/database.php';
 include "response.php";
 
 $data = json_decode(file_get_contents("php://input"), true);
